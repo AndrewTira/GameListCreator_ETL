2cd4f2f24a34f1b521a09713045a9e5316de8911
Andrew Tirado <andrewt@debian-andrew.chicowifi.com>
2025-05-17 10:45:29 -0400

    switched back to bitwarden

--- docker-scripts/ETL.py
+++ docker-scripts/ETL.py
@@ -31,67 +31,65 @@ class GameListCreator:
         else:
             print(f"Error: {response.status_code}")
 
-    def stepone(self):
-        if owned_games:
-            owned_games_df = pl.DataFrame(None,
-                                          schema=
-                                          {
-                                              "game_name": pl.String,
-                                              "hltb_game_name": pl.String,
-                                              "completionist_time": pl.Float64,
-                                              "found": pl.Boolean,
-                                              "same": pl.Boolean
-                                          })
-            game_name = 'TEMP VALUE'
-            completion_time = 0.0
-            print("There are " + str(len(owned_games)) + " games in your Steam library.")
-            for i, game in enumerate(owned_games):
-
-                if i > 10:
-                    break
-                result_list = HowLongToBeat().search(game['name'])
-                if result_list is not None and len(result_list) > 0:
-                    # make sure its from steam too
-                    best_element = max(result_list, key=lambda element: element.similarity)
-                    print(str(i) + ": " + best_element.game_name + " loaded")
-                    game_name = best_element.game_name
-                    completion_time = best_element.completionist
-                    found_bool = True
-                else:
-                    print(str(i) + ": " + game['name'] + " not loaded")
-                    game_name = 'TEMP VALUE'
-                    completion_time = 0.0
-                    found_bool = False
-
-                if game['name'] == game_name:
-                    same_bool = True
-                else:
-                    print(game['name'] + " is different than " + game_name)
-                    same_bool = False
-                new_row = \
-                    {
-                        "game_name": game['name'],
-                        "hltb_game_name": game_name,
-                        "completionist_time": completion_time,
-                        "found": found_bool,
-                        "same": same_bool
-
-                    }
-                df_new_row = pl.DataFrame(new_row)
-                owned_games_df = owned_games_df.vstack(df_new_row)
-
-                # print(f"- {game['name']}")
-        else:
-            print("No games found in the library.")
+    # def stepone(self):
+    #     if owned_games:
+    #         owned_games_df = pl.DataFrame(None,
+    #                                       schema=
+    #                                       {
+    #                                           "game_name": pl.String,
+    #                                           "hltb_game_name": pl.String,
+    #                                           "completionist_time": pl.Float64,
+    #                                           "found": pl.Boolean,
+    #                                           "same": pl.Boolean
+    #                                       })
+    #         game_name = 'TEMP VALUE'
+    #         completion_time = 0.0
+    #         print("There are " + str(len(owned_games)) + " games in your Steam library.")
+    #         for i, game in enumerate(owned_games):
+    #
+    #             if i > 10:
+    #                 break
+    #             result_list = HowLongToBeat().search(game['name'])
+    #             if result_list is not None and len(result_list) > 0:
+    #                 # make sure its from steam too
+    #                 best_element = max(result_list, key=lambda element: element.similarity)
+    #                 print(str(i) + ": " + best_element.game_name + " loaded")
+    #                 game_name = best_element.game_name
+    #                 completion_time = best_element.completionist
+    #                 found_bool = True
+    #             else:
+    #                 print(str(i) + ": " + game['name'] + " not loaded")
+    #                 game_name = 'TEMP VALUE'
+    #                 completion_time = 0.0
+    #                 found_bool = False
+    #
+    #             if game['name'] == game_name:
+    #                 same_bool = True
+    #             else:
+    #                 print(game['name'] + " is different than " + game_name)
+    #                 same_bool = False
+    #             new_row = \
+    #                 {
+    #                     "game_name": game['name'],
+    #                     "hltb_game_name": game_name,
+    #                     "completionist_time": completion_time,
+    #                     "found": found_bool,
+    #                     "same": same_bool
+    #
+    #                 }
+    #             df_new_row = pl.DataFrame(new_row)
+    #             owned_games_df = owned_games_df.vstack(df_new_row)
+    #
+    #             # print(f"- {game['name']}")
+    #     else:
+    #         print("No games found in the library.")
 if __name__ == "__main__":
+    gameListCreator = GameListCreator()
 
-   gameListCreator = GameListCreator()
-
-
-    insert_df = owned_games_df.filter(pl.col("found"))
-    insert_df = insert_df.filter(pl.col("same"))
-    insert_df = insert_df.drop("hltb_game_name")
-    insert_df = insert_df.drop("found")
-    insert_df = insert_df.drop("same")
-    insert_df.write_database(table_name="games.owned_games_list", connection=connectionString, if_table_exists = 'append'
-)
+    print(GameListCreator.STEAM_API_KEY)
+    # insert_df = owned_games_df.filter(pl.col("found"))
+    # insert_df = insert_df.filter(pl.col("same"))
+    # insert_df = insert_df.drop("hltb_game_name")
+    # insert_df = insert_df.drop("found")
+    # insert_df = insert_df.drop("same")
+    # insert_df.write_database(table_name="games.owned_games_list", connection=connectionString, if_table_exists = 'append'
